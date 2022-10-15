2866924dae9e5e2bf7b4f983bdceee1a2e97d8fe
John Khachian <jaksawesome10@gmail.com>
2022-10-15 12:30:03 -0400

    UI stuff

--- src/components/single-use/Window/ChatBox/ChatBox.js
+++ src/components/single-use/Window/ChatBox/ChatBox.js
@@ -1,10 +1,35 @@
 import "./ChatBox.css";
 
 const ChatBox = () => {
+    const messages = [];
+    const ayos = [
+        "./ayos/0.png",
+        "./ayos/1.png",
+        "./ayos/2.png",
+    ];
+
     return (
         <div id="chat-box">
-            <div id="message-view"></div>
-            <div id="emoji-bar"></div>
+            <ul id="message-view">
+                {
+                    messages.map(message => (
+                        <li>
+                            <span id="author">{message.author}</span>
+                            <span> : </span>
+                            <span id="text">{message.text}</span>
+                        </li>
+                    ))
+                }
+            </ul>
+            <div id="emoji-bar">
+                {
+                    ayos.map(a => (
+                        <a>
+                            <img src={a} alt={a} ></img>
+                        </a>
+                    ))
+                }
+            </div>
             <form id="message-input">
                 <input type="text" placeholder="type here..."/>
                 <button>Send</button>
