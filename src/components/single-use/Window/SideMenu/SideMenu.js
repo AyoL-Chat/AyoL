79c229db488cce7409580734c432332b5d75be0b
John Khachian <jaksawesome10@gmail.com>
2022-10-15 11:43:36 -0400

    people here box

--- src/components/single-use/Window/SideMenu/SideMenu.js
+++ src/components/single-use/Window/SideMenu/SideMenu.js
@@ -1,9 +1,22 @@
 import "./SideMenu.css";
 
 const SideMenu = () => {
-    return (
-        <div>
+    let activeUsers = [];
 
+    return (
+        <div id="side-menu">
+            <div id="people-here">
+                <div id="label">
+                    <span>People Here</span>
+                </div>
+                <ul id="box">
+                    {
+                        activeUsers.map(user => (
+                            <li>{user.username}</li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
